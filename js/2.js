6e52a2a424f9f7ddd54959a2783768234c794753
Ben Smith <bengsmith18@gmail.com>
2023-02-28 20:11:21 -0800

    added prev button

--- js/2.js
+++ js/2.js
@@ -1,6 +1,7 @@
 
 const baseURL = 'images/';
 const nxt = document.querySelector('.nxt');
+const prev = document.querySelector('.prev');
 const slide = document.querySelector('.pic');
 const pics = ['pic1.png', 'pic2.png', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg', 'pic6.jpg', 'pic7.jpg', 'pic8.jpg', 'pic9.jpg'];
 let index = 1;
@@ -9,7 +10,7 @@ nxt.onclick = function(e) {
   e.preventDefault();
   
   slide.src = baseURL + pics[index];
-  index = index + 1;
+  index++;
   console.log(index);
 
   //write a conditional so that the images wrap back to the beginning image.
@@ -19,3 +20,15 @@ nxt.onclick = function(e) {
 }
 
 //Add a previous button event listener
+prev.onclick = function(e) {
+    e.preventDefault();
+    
+    slide.src = baseURL + pics[index];
+    index--
+    console.log(index);
+  
+    //write a conditional so that the images wrap back to the beginning image.
+    if(index <= 0) {
+      index = 8;
+    }
+  }
