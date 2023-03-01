2bfd5703b052086523e4915587c37854838eec30
Ben Smith <bengsmith18@gmail.com>
2023-02-28 19:57:18 -0800

    first commit

---
+++ js/2.js
@@ -0,0 +1,21 @@
+
+const baseURL = 'images/';
+const nxt = document.querySelector('.nxt');
+const slide = document.querySelector('.pic');
+const pics = ['pic1.png', 'pic2.png', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg', 'pic6.jpg', 'pic7.jpg', 'pic8.jpg', 'pic9.jpg'];
+let index = 1;
+
+nxt.onclick = function(e) {
+  e.preventDefault();
+  
+  slide.src = baseURL + pics[index];
+  index = index + 1;
+  console.log(index);
+
+  //write a conditional so that the images wrap back to the beginning image.
+  if(index >= pics.length) {
+    index = 0;
+  }
+}
+
+//Add a previous button event listener
